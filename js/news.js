238d189caac34ac5bc09bd85dc698cb735665337
yji35j4212 <yji35j4212@gmail.com>
2023-03-29 15:53:39 +0800

    檔案同步

---
+++ js/news.js
@@ -0,0 +1,9 @@
+var selectHandler = document.querySelectorAll("[name=selectHandler]");
+for (let i = 0; i < selectHandler.length; i++) {
+	const element = selectHandler[i];
+	console.log("element", element);
+
+	element.onclick = function() {
+		console.log("selectHandler");
+	};
+}
