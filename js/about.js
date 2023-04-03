3980b292689b5c714e9013d29739bbf503b34c56
yji35j4212 <yji35j4212@gmail.com>
2023-04-03 17:47:14 +0800

    檔案同步

--- js/about.js
+++ js/about.js
@@ -1,34 +1,69 @@
 window.addEventListener("scroll", scrollListener);
 var business = document.querySelector("#business");
-var businessTitle1 = document.querySelector("#business_title1");
-var business_detail_txt2 = document.querySelector(".business_detail_txt2");
+var anishow = document.querySelectorAll("[name=anishow]");
+var showCount = 0;
+var range = 0;
+console.log("anishow", anishow);
+
+for (let i = 0; i < anishow.length; i++) {
+	const element = anishow[i];
+	console.log("anishow" + i + ":" + element.offsetTop);
+}
+
 function scrollListener() {
 	var windowHeight = window.pageYOffset;
-	var firstRange = businessTitle1.offsetTop + business.offsetTop;
-	var secondRange = business_detail_txt2.offsetTop + firstRange;
 	console.log("windowHeight", windowHeight);
-	console.log("firstRange", firstRange);
-	if (windowHeight + 300 > firstRange) {
-		business.classList.add("businessTitle1");
-	} else if (windowHeight < firstRange - 487) {
-		business.classList.remove("businessTitle1");
+	console.log("business", business.offsetTop);
+	let count = parseInt(showCount) + parseInt(1);
+	if (
+		windowHeight + 300 >
+		anishow[showCount].offsetTop + business.offsetTop
+	) {
+		business.classList.add("businessTitle" + count);
+	} else if (
+		windowHeight <
+		anishow[showCount].offsetTop + business.offsetTop - 487
+	) {
+		business.classList.remove("businessTitle" + count);
 	}
-
-	if (windowHeight + 300 > secondRange) {
-		business.classList.add("businessTitle2");
-	} else if (windowHeight < secondRange - 487) {
-		business.classList.remove("businessTitle2");
-	}
-	// console.log('business_detail_txt2',business_detail_txt2.offsetTop);
-
-	// if (windowHeight + 300 > businessTitle1.offsetTop + business.offsetTop) {
-	//
-	// 	console.log("出現");
-	// } else if (
-	//
-	//
-	// ) {
-	//
-	// 	console.log("消失");
-	// }
 }
+
+//
+// var businessTitle1 = document.querySelector("#business_title1");
+// var business_detail_txt2 = document.querySelector(".business_detail_txt2");
+// var business_title2 = document.querySelector("#business_title2");
+// var business_detail_txt3 = document.querySelector("#business_detail_txt3");
+
+// function scrollListener() {
+// 	var windowHeight = window.pageYOffset;
+// 	var firstRange = businessTitle1.offsetTop + business.offsetTop;
+// 	var secondRange = business_detail_txt2.offsetTop + firstRange;
+// 	var thirdRange = business_title2.offsetTop + firstRange;
+// 	var forRange = business_detail_txt3.offsetTop + firstRange;
+
+// 	console.log("windowHeight", windowHeight);
+// 	console.log("firstRange", firstRange);
+// 	if (windowHeight + 300 > firstRange) {
+// 		business.classList.add("businessTitle1");
+// 	} else if (windowHeight < firstRange - 487) {
+// 		business.classList.remove("businessTitle1");
+// 	}
+
+// 	if (windowHeight + 300 > secondRange) {
+// 		business.classList.add("businessTitle2");
+// 	} else if (windowHeight < secondRange - 487) {
+// 		business.classList.remove("businessTitle2");
+// 	}
+
+// 	if (windowHeight + 300 > thirdRange) {
+// 		business.classList.add("businessTitle3");
+// 	} else if (windowHeight < thirdRange - 487) {
+// 		business.classList.remove("businessTitle3");
+// 	}
+
+// 	if (windowHeight + 300 > forRange) {
+// 		business.classList.add("businessTitle4");
+// 	} else if (windowHeight < forRange - 487) {
+// 		business.classList.remove("businessTitle4");
+// 	}
+// }
