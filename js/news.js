9ec1291eeb759982fc6b603e12d3f80641856065
yji35j4212 <yji35j4212@gmail.com>
2023-04-12 13:27:37 +0800

    檔案同步

--- js/news.js
+++ js/news.js
@@ -3,7 +3,7 @@ var page_input = document.querySelectorAll("[name=page_input]");
 var selectHandlerItem = null;
 for (let i = 0; i < selectHandler.length; i++) {
 	const element = selectHandler[i];
-	element.onclick = function () {
+	element.onclick = function() {
 		if (selectHandlerItem == i) {
 			selectHandler[selectHandlerItem].classList.remove("on");
 			selectHandlerItem = null;
@@ -20,10 +20,9 @@ for (let i = 0; i < selectHandler.length; i++) {
 
 var page_group_more = document.querySelectorAll("[name=page_group_more]");
 var more_item = null;
-
 for (let i = 0; i < page_group_more.length; i++) {
 	const element = page_group_more[i];
-	element.onclick = function () {
+	element.onclick = function() {
 		if (more_item == i) {
 			page_group_more[more_item].classList.remove("on");
 			more_item = null;
@@ -39,10 +38,18 @@ for (let i = 0; i < page_group_more.length; i++) {
 		}
 	};
 }
+for (let i = 0; i < page_input.length; i++) {
+	const element = page_input[i];
+	element.addEventListener("blur", function() {
+		page_group_more[more_item].classList.remove("on");
+		more_item = null;
+	});
+}
+
 var page_goPage = document.querySelectorAll("[name=page_goPage]");
 for (let i = 0; i < page_goPage.length; i++) {
 	const element = page_goPage[i];
-	element.onclick = function () {
+	element.onclick = function() {
 		event.stopPropagation();
 	};
 }
@@ -54,43 +61,43 @@ var pageCount = null;
 
 for (let i = 0; i < head_page.length; i++) {
 	const element = head_page[i];
-	if (element.classList.contains('on')) { pageCount = i }
-	element.onclick = function () {
-		pageHandler(i)
+	if (element.classList.contains("on")) {
+		pageCount = i;
 	}
+	element.onclick = function() {
+		pageHandler(i);
+	};
 }
 for (let i = 0; i < footer_page.length; i++) {
 	const element = footer_page[i];
-	element.onclick = function () {
-		pageHandler(i)
-	}
+	element.onclick = function() {
+		pageHandler(i);
+	};
 }
 function pageHandler(addNum) {
-	head_page[pageCount].classList.remove('on')
-	footer_page[pageCount].classList.remove('on')
-	head_page[addNum].classList.add('on')
-	footer_page[addNum].classList.add('on')
-	pageCount = addNum
+	head_page[pageCount].classList.remove("on");
+	footer_page[pageCount].classList.remove("on");
+	head_page[addNum].classList.add("on");
+	footer_page[addNum].classList.add("on");
+	pageCount = addNum;
 }
 // page
-
-
 
 var searchInpBox = document.querySelector("#searchInpBox");
 var searchInp = document.querySelector("#searchInp");
 var searchHandler = document.querySelector("#searchHandler");
 
-searchInp.addEventListener("focus", function () {
-	searchInpBox.classList.add("_in")
-})
-searchInp.addEventListener("blur", function () {
+searchInp.addEventListener("focus", function() {
+	searchInpBox.classList.add("_in");
+});
+searchInp.addEventListener("blur", function() {
 	setTimeout(() => {
-		searchInpBox.classList.remove("_in")
+		searchInpBox.classList.remove("_in");
 	}, 100);
-})
-searchHandler.onclick = function (params) {
+});
+searchHandler.onclick = function(params) {
 	setTimeout(() => {
 		searchInp.blur();
 	}, 100);
-	searchInpBox.classList.remove("_in")
-}
+	searchInpBox.classList.remove("_in");
+};
