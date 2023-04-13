44fb2e1efb3e258ae2bde0e38b5f16595d758907
yji35j4212 <yji35j4212@gmail.com>
2023-04-13 14:17:12 +0800

    檔案同步

--- js/recruitment.js
+++ js/recruitment.js
@@ -1,25 +1,65 @@
 var recruitment_term = document.querySelectorAll("#recruitment_term > li");
 var body = document.html;
 var nav_box = document.querySelector("#nav_box");
-// var defaultGo = window.innerWidth * 0.3 - nav_box.offsetHeight;
+var join = document.querySelector("#join");
 var defaultGo = 98 + window.innerWidth * 0.3 - 66 - 103;
 var nowScroll = 0;
-console.log(" nav_box.offsetHeight", nav_box.offsetHeight);
-
+var oldScroll = 0;
+var scrollState = "down";
+var termCount = null;
+var termArr = [];
+var defaultScore = window.innerHeight * (3 / 5); //超出視窗高度的3分之1
 window.addEventListener("scroll", scrollListener);
-function scrollListener() {
-	var windowHeight = window.pageYOffset;
-	console.log("windowHeight", windowHeight);
-}
 
+console.log("defaultGo", defaultGo);
+console.log("window.scrollY", window.scrollY);
+
+function clearTerm() {
+	for (let i = 0; i < recruitment_term.length; i++) {
+		recruitment_term[i].classList.remove("on");
+	}
+}
+function scrollListener() {
+	var nowScroll = window.scrollY;
+	console.log("ddd", window.scrollY);
+	console.log("termArr[0] + defaultGo", termArr[0] + defaultGo);
+
+	nowScroll >= oldScroll ? (scrollState = "down") : (scrollState = "up");
+
+	oldScroll = nowScroll;
+	if (termCount == null && nowScroll > termArr[0] && scrollState == "down") {
+		termCount = 0;
+		recruitment_term[termCount].classList.add("on");
+	} else if (nowScroll > termArr[termCount + 1] && scrollState == "down") {
+		recruitment_term[termCount].classList.remove("on");
+		termCount++;
+		recruitment_term[termCount].classList.add("on");
+	}
+
+	if (nowScroll == 0 && termCount !== null) {
+		// recruitment_term[termCount].classList.remove("on");
+		// termCount = 0;
+		// recruitment_term[termCount].classList.add("on");
+	} else if (termCount == 1 && termArr[0] + defaultGo + 50 > nowScroll) {
+		recruitment_term[termCount].classList.remove("on");
+		termCount = 0;
+		recruitment_term[termCount].classList.add("on");
+	} else if (
+		termCount !== 0 &&
+		termArr[termCount] - defaultScore > nowScroll &&
+		scrollState == "up"
+	) {
+		recruitment_term[termCount].classList.remove("on");
+		termCount--;
+		recruitment_term[termCount].classList.add("on");
+	}
+}
 for (let i = 0; i < recruitment_term.length; i++) {
 	const element = recruitment_term[i];
 	element.onclick = function() {
 		var item = document.querySelector("#" + element.getAttribute("name"));
 		var nowScroll = window.scrollY;
 		var goScroll = item.offsetTop + defaultGo;
-		console.log("top" + goScroll);
-
 		goScroll > nowScroll
 			? goDown(nowScroll, goScroll)
 			: goTop(nowScroll, goScroll);
@@ -70,3 +110,25 @@ select("#mailHandler").addEventListener("click", e => {
 		mailHandler.classList.remove("on");
 	}, 2500);
 });
+
+function getItemTop() {
+	var nowScroll = window.scrollY;
+	termArr = [];
+	for (let i = 0; i < recruitment_term.length; i++) {
+		const element = recruitment_term[i];
+		var item = document.querySelector("#" + element.getAttribute("name"));
+		termArr.push(item.offsetTop);
+		if (nowScroll > item.offsetTop) {
+			termCount = i;
+		}
+	}
+	if (termCount == null) return;
+	recruitment_term[termCount].classList.add("on");
+	console.log("termArr", termArr);
+	console.log("termCount", termCount);
+}
+
+setTimeout(() => {
+	getItemTop();
+	scrollListener();
+}, 300);
