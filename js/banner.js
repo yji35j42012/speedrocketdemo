92d026952ca62915e753e927045612aba3570396
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2023-04-23 20:57:53 +0800

    檔案同步

---
+++ js/banner.js
@@ -0,0 +1,149 @@
+var banner = document.querySelector("#banner_group");
+var banner_pic = document.querySelectorAll("#banner_group > .banner_item");
+var banner_prev = document.querySelector("#banner_prev");
+var banner_next = document.querySelector("#banner_next");
+var banner_count = 1;
+var banner_moveNum = -100 * banner_count;
+var banner_maxCount = banner_pic.length;
+var banner_time = 6; //6秒
+let bannerTime = null;
+var banner_dots = document.querySelector("#banner_dots");
+var banner_dots_item = null;
+var photo_kp = document.querySelector("#photo_kp");
+var introduction1 = document.querySelector("#introduction1");
+var introduction1_pic = document.querySelector(
+	"#introduction1 > .brandSH_introduction_pic"
+);
+var introduction1_txt = document.querySelector(
+	"#introduction1 > .brandSH_introduction_txt"
+);
+
+function bannerTimeHandler() {
+	bannerTime = setInterval(() => {
+		banner_count++;
+		banner_moveNum = -100 * banner_count;
+		banner_moveHandler();
+		dotsHandler();
+		resetTime();
+	}, banner_time * 1000);
+}
+banner_next.onclick = function () {
+	banner_count++;
+	banner_moveNum = -100 * banner_count;
+	banner_moveHandler();
+	resetTime();
+	dotsHandler();
+};
+banner_prev.onclick = function () {
+	banner_count--;
+	banner_moveNum = -100 * banner_count;
+	banner_moveHandler();
+	resetTime();
+	dotsHandler();
+};
+function resetTime() {
+	clearInterval(bannerTime);
+	bannerTimeHandler();
+	if (banner_count == banner_maxCount + 1) {
+		goFirst();
+	} else if (banner_count == 0) {
+		goEnd();
+	}
+}
+// 快速換回第一張
+function goFirst() {
+	setTimeout(() => {
+		banner_count = 1;
+		banner_moveNum = -100 * banner_count;
+		banner.style = `transform: translateX(${banner_moveNum}%);transition-duration: 0;opacity:1;`;
+	}, 350);
+}
+// 快速換回最後一張
+function goEnd() {
+	setTimeout(() => {
+		banner_count = banner_maxCount;
+		banner_moveNum = -100 * banner_count;
+		banner.style = `transform: translateX(${banner_moveNum}%);transition-duration: 0;opacity:1;`;
+	}, 350);
+}
+function banner_moveHandler() {
+	banner.style = `transform: translateX(${banner_moveNum}%);transition-duration: 0.3s;opacity:1;`;
+}
+function pushStart() {
+	var getImg = document.querySelector(
+		"#banner_group > .banner_item:last-child img"
+	);
+	const liStart = document.createElement("li");
+	liStart.setAttribute("class", "banner_item");
+	liStart.setAttribute(
+		"style",
+		"background-image: url('" + getImg.getAttribute("src") + "')"
+	);
+	const child = document.createElement("img");
+	child.setAttribute("src", getImg.getAttribute("src"));
+	liStart.append(child);
+	banner.insertBefore(liStart, banner_pic[0]);
+}
+function pushEnd() {
+	var getImg = document.querySelector(
+		"#banner_group > .banner_item:nth-child(2) img"
+	);
+	const liEnd = document.createElement("li");
+	liEnd.setAttribute("class", "banner_item");
+	liEnd.setAttribute(
+		"style",
+		"background-image: url('" + getImg.getAttribute("src") + "')"
+	);
+	const child = document.createElement("img");
+	child.setAttribute("src", getImg.getAttribute("src"));
+	liEnd.append(child);
+	banner.appendChild(liEnd, banner_pic[0]);
+}
+function pushDots() {
+	for (let i = 0; i < banner_maxCount; i++) {
+		const liDot = document.createElement("li");
+		liDot.setAttribute("class", "normal_dots_item");
+		banner_dots.append(liDot);
+	}
+	banner_dots_item = document.querySelectorAll("#banner_dots >li");
+	dotsHandler();
+	dotItem();
+}
+function dotsHandler() {
+	allDotsRemove();
+	if (banner_count == 1) {
+		banner_dots_item[banner_count - 1].classList.add("on");
+	} else if (banner_count == 0) {
+		banner_dots_item[banner_maxCount - 1].classList.add("on");
+	} else if (banner_count > banner_maxCount) {
+		banner_dots_item[0].classList.add("on");
+	} else {
+		banner_dots_item[banner_count - 1].classList.add("on");
+	}
+}
+function dotItem() {
+	for (let i = 0; i < banner_dots_item.length; i++) {
+		const element = banner_dots_item[i];
+		element.onclick = function () {
+			banner_count = i + 1;
+			banner_moveNum = -100 * banner_count;
+			banner_moveHandler();
+			dotsHandler();
+			resetTime();
+		};
+	}
+}
+function allDotsRemove() {
+	for (let i = 0; i < banner_dots_item.length; i++) {
+		const element = banner_dots_item[i];
+		element.classList.remove("on");
+	}
+}
+setTimeout(() => {
+	pushStart();
+	pushEnd();
+	banner_moveHandler();
+	pushDots();
+}, 100);
+
+bannerTimeHandler();
