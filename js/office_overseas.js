f3698b8bb9b0acd4ca9da04da59588d8b536c240
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2023-04-27 08:22:29 +0800

    檔案同步

--- js/office_overseas.js
+++ js/office_overseas.js
@@ -16,3 +16,23 @@ searchHandler.onclick = function(params) {
 	}, 100);
 	searchInpBox.classList.remove("_in");
 };
+
+var selectHandler = document.querySelectorAll("[name=selectHandler]");
+var page_input = document.querySelectorAll("[name=page_input]");
+var selectHandlerItem = null;
+for (let i = 0; i < selectHandler.length; i++) {
+	const element = selectHandler[i];
+	element.onclick = function() {
+		if (selectHandlerItem == i) {
+			selectHandler[selectHandlerItem].classList.remove("on");
+			selectHandlerItem = null;
+		} else if (selectHandlerItem == null) {
+			selectHandler[i].classList.add("on");
+			selectHandlerItem = i;
+		} else {
+			selectHandler[selectHandlerItem].classList.remove("on");
+			selectHandler[i].classList.add("on");
+			selectHandlerItem = i;
+		}
+	};
+}
